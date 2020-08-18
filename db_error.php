33d09ac035699f4b8f1fcbfedfdb6ea10b306101
myykel25 <62095703+myykel25@users.noreply.github.com>
2020-08-18 12:22:22 +0100

    initial commit

---
+++ db_error.php
@@ -0,0 +1,12 @@
+  <!DOCTYPE html>
+  <html lang="en" dir="ltr">
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+      <h1>Database Error</h1>
+      <p><?php echo $err_msg;  ?></p>
+
+    </body>
+  </html>
